da90bba2abffdf8cd1b24e5358a9195a8eeb97b1
com-windows <none>
2022-02-18 17:33:30 +0800

    调整目录结构

---
+++ src/ftp_server.h
@@ -0,0 +1,13 @@
+/*================================================================*
+        Copyright (C) 2021 All rights reserved, www.hsby.link.
+      	文件名称：ftp_server.h
+      	创 建 者：hsby
+      	创建日期：2022/2/18
+ *================================================================*/
+#pragma once
+
+#include <xco/tcp_server.h>
+
+class FtpServer : public xco::TcpServer {
+
+};
